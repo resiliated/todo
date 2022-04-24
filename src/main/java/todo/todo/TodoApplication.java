85360939307832aee8b3ced666ffb754800583da
resiliated <boris.raimbault.37@gmail.com>
2022-04-24 19:50:32 +0200

    Initial commit

---
+++ src/main/java/todo/todo/TodoApplication.java
@@ -0,0 +1,14 @@
+package todo.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		System.
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
